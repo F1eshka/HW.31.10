b732c5dec9e91eac7f511cead0c35ad6308bd5f3
F1eshka <144143888+F1eshka@users.noreply.github.com>
2024-12-05 13:03:30 +0300

    Add files via upload

---
+++ Cell.cs
@@ -0,0 +1,55 @@
+﻿namespace Maze
+{
+    public class Character
+    {
+        public ushort PosX { get; set; }
+        public ushort PosY { get; set; }
+        public LevelForm Parent { get; set; }
+        public int MedalCount { get; set; }
+        public int StepCount { get; set; }
+        public int AmountOfHealth { get; set; }
+
+        public Character(LevelForm parent)
+        {
+            PosX = 0;
+            PosY = 2;
+            Parent = parent;
+        }
+
+        public void Clear()
+        {
+            Parent.Controls["pic" + PosY + "_" + PosX].BackgroundImage =
+                Parent.maze.cells[PosY, PosX].Texture =
+                Cell.Images[(int)(Parent.maze.cells[PosY, PosX].Type = CellType.HALL)];
+        }
+
+      
+
+        public void Move(int deltaY, int deltaX)
+        {
+            PosY = (ushort)(PosY + deltaY);
+            PosX = (ushort)(PosX + deltaX);
+        }
+
+        public void MoveRight() => Move(0, 1);
+        public void MoveLeft() => Move(0, -1);
+        public void MoveUp() => Move(-1, 0);
+        public void MoveDown() => Move(1, 0);
+
+        public void Show()
+        {
+            Parent.Controls["pic" + PosY + "_" + PosX].BackgroundImage =
+                Parent.maze.cells[PosY, PosX].Texture =
+                Cell.Images[(int)(Parent.maze.cells[PosY, PosX].Type = CellType.HERO)];
+        }
+        public bool CheckVictory()
+        {
+            if (Parent.Hero.PosX == Configuration.Columns - 1 && Parent.Hero.PosY == Configuration.Rows - 3)
+            {
+                MessageBox.Show("Victory - A way out");
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Maze.cs
@@ -0,0 +1,84 @@
+﻿namespace Maze
+{
+    public class Maze
+    {
+        public LevelForm Parent { get; set; } 
+        public Cell[,] cells;
+        public static Random r = new Random();
+
+        public Maze(LevelForm parent)
+        {
+            Parent = parent;
+            cells = new Cell[Configuration.Rows, Configuration.Columns];
+        }
+
+        public void Generate()
+        {
+            for (ushort row = 0; row < Configuration.Rows; row++)
+            {
+                for (ushort col = 0; col < Configuration.Columns; col++)
+                {
+                    CellType cell = CellType.HALL;
+
+                    if (r.Next(5) == 0)
+                    {
+                        cell = CellType.WALL;
+                    }
+
+                    if (r.Next(250) == 0)
+                    {
+                        cell = CellType.MEDAL;
+                    }
+
+                    if (r.Next(250) == 0)
+                    {
+                        cell = CellType.ENEMY;
+                    }
+
+                    if (row == 0 || col == 0 ||
+                        row == Configuration.Rows - 1 ||
+                        col == Configuration.Columns - 1)
+                    {
+                        cell = CellType.WALL;
+                    }
+
+                    if (col == Parent.Hero.PosX &&
+                        row == Parent.Hero.PosY)
+                    {
+                        cell = CellType.HERO;
+                    }
+
+                    if (col == Parent.Hero.PosX + 1 &&
+                        row == Parent.Hero.PosY ||
+                        col == Configuration.Columns - 1 &&
+                        row == Configuration.Rows - 3)
+                    {
+                        cell = CellType.HALL;
+                    }
+
+                    cells[row, col] = new Cell(cell);
+
+                    var picture = new PictureBox();
+                    picture.Name = "pic" + row + "_" + col;
+                    picture.Width = Configuration.PictureSide;
+                    picture.Height = Configuration.PictureSide;
+                    picture.Location = new Point(
+                        col * Configuration.PictureSide,
+                        row * Configuration.PictureSide);
+
+                    picture.BackgroundImage = cells[row, col].Texture;
+                    picture.Visible = false;
+                    Parent.Controls.Add(picture);
+                }
+            }
+        }
+        public void Show()
+        {
+            foreach (var control in Parent.Controls)
+            {
+                if (control is PictureBox)
+                    ((PictureBox)control).Visible = true;
+            }
+        }
+    }
+}
